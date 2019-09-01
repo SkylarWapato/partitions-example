58cea1528883bde67c9b8ef17d41b2f1e2aed202
skylarwapato <skylar.wapato@gmail.com>
2019-09-01 01:38:20 -0500

    chore: prep starbucks branch

---
+++ seed.js
@@ -0,0 +1,39 @@
+const csv = require('csvtojson')
+const concurrencyMap = require('bluebird').map
+const request = require('graphql-request').request
+
+const dataFilePath = 'starbucks_us_locations.csv'
+const graphqlEndpoint = 'https://cwpeyby31d.execute-api.us-east-1.amazonaws.com/dev/graphql'
+const mutation = /* GraphQL */ `
+mutation ($data: StarbucksCreateInput!, $latitude: Float!, $longitude: Float!) {
+    createGeoScopedStarbucks(data: $data, latitude: $latitude, longitude: $longitude)
+  }`
+
+// If a request fails due to stress just set this to the last executed index from the log
+let startIndex = 0
+
+const run = async () => {
+
+    const data = await csv().fromFile(dataFilePath)
+    const count = data.length
+    for (let i = startIndex; i < count; i++) {
+        const request = require('graphql-request').request
+        if(!data[i]) continue
+        const args = {
+            latitude: data[i].lat ? parseFloat(data[i].lat) : 0,
+            longitude: data[i].lon ? parseFloat(data[i].lon) : 0,
+            data: {
+                address: data[i].address ? data[i].address : 'N/A',
+                storeId: data[i].storeId ? data[i].storeId : 'N/A'
+            }
+        }
+        const req = request(graphqlEndpoint, mutation, args)
+        const res = await req
+        console.log('response:', res)
+        console.log(`${i} down ${count - i - 1} to go`)
+    }
+
+    console.log('Complete')
+}
+
+run()
